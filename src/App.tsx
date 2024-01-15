ea13b41d0b5b236b643b755c06193577c565f380
gorhf9397 <trr456@naver.com>
2024-01-15 13:21:59 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { legacy_createStore } from "redux";
 
-function App() {
+const App = () => {
+  const reducer = (state = 0) => {
+    return state;
+  };
+
+  const store = legacy_createStore(reducer);
+
+  console.log(store.getState());
+
+  // const [count, setCount] = useState<number>(0);
+  // const add = () => {
+  //   setCount(count + 1);
+  // };
+  // const minus = () => {
+  //   setCount(count - 1);
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <button id="minus" onClick={minus}>
+        마이너스
+      </button>
+      <span>{count}</span>
+      <button id="plus" onClick={add}>
+        플러스
+      </button> */}
     </div>
   );
-}
+};
 
 export default App;
